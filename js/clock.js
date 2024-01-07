0e1a435569042786953d1590fa3a98a44595b9b4
Chan-don <152566963+Chan-don@users.noreply.github.com>
2024-01-07 23:51:42 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -17,7 +17,7 @@ function getClock() {
     const minutes = String(SeoulTime.getMinutes()).padStart(2, "0");
     const seconds = String(SeoulTime.getSeconds()).padStart(2, "0");
 
-    clock.innerText = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    clock.innerText = `Now Seoul: ${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
